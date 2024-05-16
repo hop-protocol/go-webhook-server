6e4db68b6353517bde761af25f1ecbf4583c4949
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-05-15 17:39:41 -0700

    Add debug logs

--- server/server.go
+++ server/server.go
@@ -58,6 +58,8 @@ func (s *Server) Start() error {
 
 // Handler ...
 func (s *Server) Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Received %s request\n", s.method)
+	fmt.Printf("Path: %s\n", s.path)
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -65,6 +67,7 @@ func (s *Server) Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Printf("Path: %s\n", s.secret)
 	if s.secret != "" {
 		headerSig := r.Header.Get("X-Hub-Signature-256")
 		sig := hmacSig(s.secret, b)
